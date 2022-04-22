887dac547664b0a5773b7787d34c67720b2555b5
Aminul Islam <ontor2013@gmail.com>
2022-04-22 14:51:52 +0600

    issue fixed

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,6 +1,6 @@
 <?php
 $mysql_hostname = "ec2-18-140-1-70.ap-southeast-1.compute.amazonaws.com";
-$mysql_user = "phpMyAdmin";
+$mysql_user = "phpmyadmin";
 $mysql_password = "D3j#3XMCh5de3REA";
 $mysql_database = "cms";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
