5159f9a6820c23e235b4f36bec540cd5db7f72d8
Aminul Islam <ontor2013@gmail.com>
2022-04-22 14:41:58 +0600

    some change

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
+$mysql_user = "phpMyAdmin";
+$mysql_password = "D3j#3XMCh5de3REA";
 $mysql_database = "cms";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
 
