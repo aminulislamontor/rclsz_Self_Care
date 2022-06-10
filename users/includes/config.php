5392f09a92d3ade1f023d93975ba15a27a478e79
Aminul Islam <ontor2013@gmail.com>
2022-06-10 22:35:38 +0600

    Bug fix

--- users/includes/config.php
+++ users/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-$mysql_hostname = "localhost:3306";
-$mysql_user = "phpmyadmin";
-$mysql_password = "D3j#3XMCh5de3REA";
+$mysql_hostname = "localhost";
+$mysql_user = "root";
+$mysql_password = "";
 $mysql_database = "cms";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
 
